3e300b87721edc63519a6e4f69ee96e0b67b3280
techhirestudent <live_work_achieve@outlook.com>
2017-01-27 13:13:29 -0500

    raffle win

--- TestRaffleTicket.rb
+++ TestRaffleTicket.rb
@@ -3,4 +3,15 @@ winning_tickets.include? (my_ticket)
 puts my_ticket
 end
 
+def winningOnumber_each?(my_ticket, winning_tickets)
+	winner = false
+
+	winning_tickets.each do |ticket|
+		if my_ticket == ticket
+			return true
+		end
+	end
+	winner
+end
+
 #winning_number_include?("5423, ["5423")
